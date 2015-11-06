6da03a521e65b938bbdad1f2cf03b223fa008907
Jake <jwhite52@gmail.com>
2015-11-06 16:08:50 -0500

    formatting for intro links done + shows section: api querying work
    api work: handling specific start time vs all day event

--- js/calender.js
+++ js/calender.js
@@ -73,14 +73,17 @@
 
         request.execute(function(resp) {
           var events = resp.items;
-          // appendPre('Upcoming events:');
           
+          
+          // console.log(events)
+
           if (events.length > 0) {
             for (i = 0; i < 5; i++) {
-              var event = events[i];        //individual object
-              var link = event.description;    //extract object description (link to venue/tickets page)
+              var event = events[i];            //individual object
+              var link = event.description;     //extract object description (link to venue/tickets page)
 
-              appendDateTime(event.start.dateTime, event.end.dateTime, i+1)
+              // console.log(event)
+              appendDateTime(event.start, event.end, i+1)
               appendLink(link, i+1);
               appendLocation(event.summary, i+1)
 
@@ -95,8 +98,15 @@
       //send link to venue/tickets page to '<a>' tag
       function appendLink(message, count) {
         var aTag = document.getElementById('link-'+count);
+        
+        if(!message) //handle when no link is provided
+        {
+          return
+        }
         aTag.setAttribute("href", "https://" + message);
       }
+
+
       /**
        * Append date and time to appropriate places in website
        * 
@@ -107,27 +117,37 @@
       function appendDateTime(start_time, end_time, count) {
 
         //start + end time
-        var hours_minutes_array_start = []
-        var hours_minutes_array_end = []
 
-        var begin = new Date(start_time)
-        var finish = new Date(end_time)
-
-        hours_minutes_array_start[0] = begin.getHours()
-        hours_minutes_array_start[1] = ("0" + begin.getMinutes()).slice(-2)
-
-        hours_minutes_array_end[0] = finish.getHours()
-        hours_minutes_array_end[1] = ("0" + finish.getMinutes()).slice(-2)
-
-        var time_message = hours_minutes_array_start[0] + ":" + hours_minutes_array_start[1] + " - " + hours_minutes_array_end[0] + ":" + hours_minutes_array_end[1]
-
-        if(!time_message)
+        if(start_time.date) // we know that there is no specific time
         {
-          return
+          //defining variables as a date
+          var begin = new Date(start_time.date) 
+
+          document.getElementById('time-'+count).innerHTML = 'TBD';
+
         }
-        else
+        else // there is going to be a specific time of start and end
         {
+          var hours_minutes_array_start = []
+          var hours_minutes_array_end = []
+
+          //defining variables as a date
+          var begin = new Date(start_time.dateTime) 
+          var finish = new Date(end_time.dateTime)
+
+          
+          //placing variables into an array to be sent to appropriate area + getting hours out of military time
+          var hours_start = ((begin.getHours() + 11) % 12 + 1);
+          hours_minutes_array_start[0] = hours_start
+          hours_minutes_array_start[1] = ("0" + begin.getMinutes()).slice(-2)
+          
+          var hours_end = ((finish.getHours() + 11) % 12 + 1);
+          hours_minutes_array_end[0] = hours_end
+          hours_minutes_array_end[1] = ("0" + finish.getMinutes()).slice(-2)
+
+          var time_message = hours_minutes_array_start[0] + ":" + hours_minutes_array_start[1] + " - " + hours_minutes_array_end[0] + ":" + hours_minutes_array_end[1]
           document.getElementById('time-'+count).innerHTML = time_message;
+
         }
 
         // date
@@ -148,3 +168,5 @@
         var textContent = document.createTextNode(message + '\n');
         paragraph.appendChild(textContent);
       }
+
+
